086c7b82cdeaef85cfb7ff636ac0c35e54e6819a
Md Imam Hosen <mimam22.cse@bu.ac.bd>
2025-01-07 22:49:23 +0600

    Starting of Go

---
+++ GoWithHabibVai/Projects/FirstProgram/index.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+func main() {
+	fmt.Println("Hello, World! This is my first go program." )
+
+	a := 10
+	b := 20
+
+	if(a > b) {
+		fmt.Println("a is greater than b")
+	} else if(a <= b) {
+		fmt.Println("a is less than or equal to b")
+	} else {
+		fmt.Println("a is equal to b")
+	}
+}
